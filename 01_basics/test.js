0d0c6a06481ffc4477a1e1cd8446a72dfa9d569d
Harsh R Chauhan <72123867+HarshRChauhan@users.noreply.github.com>
2025-10-04 19:40:41 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,20 @@
 console.log("hello");
+
+const arr=[1,2,3,4,5];
+
+
+//   --------------------------------------------  map function  --------------------------------------------
+const square=arr.map((i)=>{
+    return i*i
+})
+console.log(square);
+console.log(arr);
+
+
+//   --------------------------------------------  forEach function  --------------------------------------------
+let sum=0
+arr.forEach(num => {
+    sum += num
+})
+console.log(sum);
+
