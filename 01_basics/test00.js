85e50a4972777f31cd8148eafa5df3914a19ecb5
Harsh R Chauhan <72123867+HarshRChauhan@users.noreply.github.com>
2025-12-21 08:59:47 +0000

    container config done

---
+++ 01_basics/test00.js
@@ -0,0 +1,15 @@
+var y=21
+var y=20                // define and assign multiple time
+// console.log(a);
+console.log(y);
+
+let x=22
+let b=24                // Define one time, assign multiple time
+console.log(b)
+console.log(x);
+
+const z=23
+const c=27              // define and assign only one time
+console.log(z);
+console.log(c);
+
